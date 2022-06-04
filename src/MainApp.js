fbb6366e5abc62954819e4253b73253690586282
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-03 23:25:19 -0500

    test pokemon api fetch helpers

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { CountFunc } from './helpers/CountFunc';
+import { pokefunc } from './helpers/pokefunc';
 import AppRoutes from './routes/AppRoutes';
 
 const MainApp = () => {
+  CountFunc().then(data=>
+      {
+        for (let index = 0; index < data; index++) {
+          pokefunc(index).then(dato=>{
+            console.log(dato)
+          })
+        }
+      }
+    )
+  
+
   return (
     <AppRoutes />
   )
