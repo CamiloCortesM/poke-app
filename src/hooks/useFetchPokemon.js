4e9dcb0bc55f330d62e1d8fb7bf72fdc12e21ca9
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-04 12:36:42 -0500

    returns the data of 100 pokemon by importing the async function

---
+++ src/hooks/useFetchPokemon.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { pokeUnique } from "../helpers/pokeUnique";
+
+export const useFetchPokemon = (poke) => {
+
+    const [pokemon, setPokemon] = useState([])
+
+    useEffect(() => {
+        poke.map(url=>{
+            return pokeUnique(url)
+            .then(data=>{
+                setPokemon(p=>[...p,data]);
+            })
+        })
+    }, [poke])
+    
+    if(pokemon.length===poke.length){
+        return pokemon
+    }
+
+    return false;
+}
