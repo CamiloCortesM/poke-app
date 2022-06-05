e2322b029937afa6a2c912f3f743cbdc88c2a64a
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-04 20:08:28 -0500

    the parameters that are returned for each pokemon have been modified.

--- src/helpers/pokeUnique.js
+++ src/helpers/pokeUnique.js
@@ -4,13 +4,18 @@ export const pokeUnique = async(link) => {
     const pokemon = {
         id: data.id,
         name: data.name,
-        url: data.sprites.other.dream_world.front_default,
+        url: data.sprites.other['official-artwork'].front_default,
+        backDefault: data.sprites.back_default,
+        backShiny:data.sprites.back_shiny,
+        frontDefault:data.sprites.front_default,
+        frontShiny:data.sprites.front_shiny,
         exp: data.base_experience,
         hp: data.stats[0].base_stat,
         atack: data.stats[1].base_stat,
         defence: data.stats[2].base_stat,
         special: data.stats[3].base_stat,
+        types: data.types,
+        weight: data.weight,
       };
-
       return pokemon;
 }
