af565e77e9646f13000bea4cfc707860fba00976
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-06 13:46:04 -0500

    returns a new array that satisfies the condition

--- src/selectors/getPokemonByType.js
+++ src/selectors/getPokemonByType.js
@@ -3,6 +3,5 @@ export const getPokemonByType = (type,data) => {
         if(type===''){
             return []
         }
-        data.map(pk =>console.log(pk.id,pk.types));
         return data.filter(poke =>poke.types[0].type.name===type || poke.types[1]?.type.name===type)
 }
