25e0b6b24908d5205b5700e448b7dbf117ad4b8f
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-07 22:03:11 -0500

    printed types of pokemon in badges

--- src/components/pokemon/CardPoket.js
+++ src/components/pokemon/CardPoket.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-export const CardPoket = ({url,id,name}) => {
+export const CardPoket = ({url,id,name,types}) => {
   return (
     <div className="col mt-3 ">
       <div className="card card-Color border-light card-poket">
         <img src={url} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name} #{id}</h5>
+          {
+            types.map(tp =>{
+              return <span key={tp.type.url} className="badge text-bg-danger me-1">{tp.type.name}</span>
+            })
+          }
         </div>
       </div>
     </div>
