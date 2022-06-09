a44a3ccafac38bd65546222a072f8c7c1a8e55b7
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-08 22:34:20 -0500

    searches for a pokemon and returns it as an object

---
+++ src/hooks/usePokemonId.js
@@ -0,0 +1,11 @@
+import { useContext}from "react";
+import { getPokemonById } from "../selectors/getPokemonById";
+import { DataContext } from "./DataContext";
+
+export const usePokemonId = (id) => {
+  const data = useContext(DataContext);
+  if (data.length === 200) {
+    const pokemon = getPokemonById(data, id);
+    return pokemon;
+  }
+};
