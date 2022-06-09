60605bbdd3dd9632038487518cd81e8453a2874f
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-08 22:37:44 -0500

    new badge style classes have been added

--- src/components/pokemon/CardPoket.js
+++ src/components/pokemon/CardPoket.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const CardPoket = ({url,id,name,types}) => {
   return (
     <div className="col mt-3 ">
+      <Link to={`/pokemon/${id}`} 
+      style={{ textDecoration: "none", color: "#000" }}>
       <div className="card card-Color border-light card-poket">
         <img src={url} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name} #{id}</h5>
           {
             types.map(tp =>{
-              return <span key={tp.type.url} className="badge text-bg-danger me-1">{tp.type.name}</span>
+              return <span key={tp.type.url} className={`badge ${tp.type.name} me-1`}>{tp.type.name}</span>
             })
           }
         </div>
       </div>
+      </Link>
     </div>
   );
 };
